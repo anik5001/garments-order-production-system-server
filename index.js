3bf6f9c4dc750bb8965c8b2732af740af87fb373
anik5001 <aniksarker047@gmail.com>
2025-12-09 12:06:40 +0600

    user collection created and users api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -28,6 +29,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("garments-order-production-system-DB");
+
+    const userCollection = db.collection("users");
+
+    // user api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      user.createdAt = new Date();
+      user.status = "pending";
+      const email = user.email;
+      const userExits = await userCollection.findOne({ email });
+      if (userExits) {
+        return res.send({ message: "user Exits" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
